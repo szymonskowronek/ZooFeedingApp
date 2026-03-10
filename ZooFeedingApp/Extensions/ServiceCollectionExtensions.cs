255c60d44411004594dade169abc789228902160
Szymon Skowronek <szymon.skowronek@globallogic.com>
2026-03-10 14:59:43 +0100

    Service registration extension method added

---
+++ ZooFeedingApp/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using ZooFeedingApp.Services.Implementations;
+using ZooFeedingApp.Services.Interfaces;
+
+namespace ZooFeedingApp.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddZooServices(this IServiceCollection services)
+        {
+            services.AddTransient<IPriceProvider, TextPriceProvider>();
+            services.AddTransient<ISpeciesProvider, CsvSpeciesProvider>();
+            services.AddTransient<IZooLoader, XmlZooLoader>();
+            services.AddTransient<IFeedingCalculator, FeedingCalculator>();
+
+            return services;
+        }
+    }
+}
